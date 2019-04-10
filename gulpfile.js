a0db4c498cabc75ddfa98125715f746b47a2dda3
asta4pasta <asta4pasta@gmail.com>
2019-04-10 21:46:11 +0300

    dar tvarkyta struktura, neivestas fontas

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var browserSync = require('browser-sync').create();
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
-      baseDir: 'app'
+      baseDir: 'app/'
     },
   })
 })
